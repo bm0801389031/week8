c6fbcde28098c7139d807d95db65ffb0bf0a6543
Brian Monge <bm0801389031@gmail.com>
2021-02-22 20:17:33 -0700

    complete file with setup final readme and testcase

---
+++ willYouMakeIt/final/logic.js
@@ -0,0 +1,13 @@
+//function statement named zeroFuel with parameters
+//distanceToPump, mpg, fuelLeft
+const zeroFuel = (distanceToPump, mpg, fuelLeft)=> {
+    //if mpg times fuelLeft greaterequal distanceToPump
+      if (mpg * fuelLeft >= distanceToPump){
+        //return true
+        return true
+        //else
+      }else{
+        //return false
+        return false
+      }
+    };
---
+++ willYouMakeIt/setup/logic.js
@@ -0,0 +1,5 @@
+//function statement named zeroFuel with parameters
+//distanceToPump, mpg, fuelLeft
+const zeroFuel = (distanceToPump, mpg, fuelLeft)=> {
+
+    };
