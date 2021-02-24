c0ed294354a445e18092f789aefc41f030deaf56
Brian Monge <bm0801389031@gmail.com>
2021-02-24 15:06:10 -0700

    filecomplet is string to upper case

---
+++ isTheStringUpperCase/final/logic.js
@@ -0,0 +1,6 @@
+//create a function called isUpperCase
+//string.potototype.isUpperCase assignment function
+String.prototype.isUpperCase = function(){
+    //return this equals this property toUpperCase method
+      return this == this.toUpperCase()
+    }
---
+++ isTheStringUpperCase/setup/logic.js
@@ -0,0 +1,5 @@
+//create a function called isUpperCase
+//string.potototype.isUpperCase assignment function
+String.prototype.isUpperCase = function(){
+
+    }
